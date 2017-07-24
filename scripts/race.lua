83700d7a5fb1b2b5e560b496a64c13933ef89865
Tanner Mckenney <tmckenney7@outlook.com>
2017-07-23 21:01:36 -0400

    UI Overhaul

--- scripts/race.lua
+++ scripts/race.lua
@@ -1,11 +1,16 @@
+-- LuaDC version 0.9.20
+-- 8/18/2014 11:54:39 PM
+-- LuaDC by Age2uN
+-- on error send source file (compiled lua) and this outputfile to Age2uN@gmx.net
+--
 NotPlayable = 0
 Playable = 1
-races =
-    {
-        { "Invalid", "Invalid", "", "", 0, NotPlayable, "", },
-        { "Hiigaran", "UNCG", "SlipstreamGate", "etg/special/SPECIAL_ABILITIES_HYPERSPACE_IN", 2, Playable, "HGN_", },
-        { "Vaygr", "DSCG", "SlipstreamGate", "etg/special/SPECIAL_ABILITIES_HYPERSPACE_IN", 2, Playable, "VGR_", },
-        { "Keeper", "$1102", "SlipstreamGate", "etg/special/SPECIAL_ABILITIES_HYPERSPACE_IN", 2, NotPlayable, "KPR_", },
-        { "Bentusi", "$1103", "SlipstreamGate", "etg/special/SPECIAL_ABILITIES_HYPERSPACE_IN", 2, NotPlayable, "BEN_", },
-        { "Random", "$3226", "", "", 0, Playable, "", },
+races = 
+    { 
+        { "Invalid", "Invalid", "", "", 0, NotPlayable, "", }, 
+        { "Hiigaran", "$1100", "hyperspace_gate", "etg/special/SPECIAL_ABILITIES_HYPERSPACE_IN", 9.5, Playable, "HGN_", }, 
+        { "Vaygr", "$1101", "hyperspace_gate_vgr", "etg/special/SPECIAL_ABILITIES_HYPERSPACE_IN", 9.5, Playable, "VGR_", }, 
+        { "Keeper", "$1102", "hyperspace_gate_kpr", "etg/special/SPECIAL_ABILITIES_HYPERSPACE_IN", 12.5, NotPlayable, "KPR_", }, 
+        { "Bentusi", "$1103", "hyperspace_gate_bentusi", "etg/special/SPECIAL_ABILITIES_HYPERSPACE_IN", 18.5, NotPlayable, "BEN_", }, 
+        { "Random", "$3226", "", "", 0, Playable, "", }, 
     }
