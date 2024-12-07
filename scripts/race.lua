98aef5178c9f1eb17e76c158772d80e01ce3fe5a
Tanner Mckenney <tmckenney7@outlook.com>
2024-12-06 22:38:22 -0500

    Change the race names back

--- scripts/race.lua
+++ scripts/race.lua
@@ -3,8 +3,8 @@ Playable = 1
 races = 
     { 
         { "Invalid", "Invalid", "", "", 0, NotPlayable, "", }, 
-        { "Hiigaran", "UNCG", "SlipstreamGate", "etg/special/special_abilities_slipstream", 2, Playable, "HGN_", }, 
-        { "Vaygr", "DSCG", "SlipstreamGate", "etg/special/special_abilities_slipstream", 2, Playable, "VGR_", }, 
+        { "Hiigaran", "$1100", "SlipstreamGate", "etg/special/special_abilities_slipstream", 2, Playable, "HGN_", }, 
+        { "Vaygr", "$1101", "SlipstreamGate", "etg/special/special_abilities_slipstream", 2, Playable, "VGR_", }, 
         { "Keeper", "$1102", "hyperspace_gate_kpr", "etg/special/SPECIAL_ABILITIES_HYPERSPACE_IN", 12.5, NotPlayable, "KPR_", }, 
         { "Bentusi", "$1103", "hyperspace_gate_bentusi", "etg/special/SPECIAL_ABILITIES_HYPERSPACE_IN", 18.5, NotPlayable, "BEN_", }, 
         { "Random", "$3226", "", "", 0, Playable, "", }, 
