6484f5b226779226ad257e7871d2ebd2040050c7
Tanner Mckenney <tmckenney7@outlook.com>
2024-12-19 18:04:00 -0500

    Format `background.lua`

--- ui/newui/main/new/background.lua
+++ ui/newui/main/new/background.lua
@@ -4,20 +4,20 @@ Background =
    stylesheet = "HW2StyleSheet",
    RootElementSettings = { },
    pixelUVCoords = 1,
-;
-{
-   type = "Frame",
-   name = "bgimage",
-   visible = 1,
-   position = {0, 0},
-   size = {800, 600},
-   BackgroundGraphic = 
+   ;
    {
-	type = "Graphic",
-	size = {800, 600},
-	color = { 255, 255, 255, 255, },
-	textureUV = {0,0,1600,1200},
-	texture = "Data:UI\\NewUI\\Textures\\background.tga",
+      type = "Frame",
+      name = "bgimage",
+      visible = 1,
+      position = {0, 0},
+      size = {800, 600},
+      BackgroundGraphic = 
+      {
+         type = "Graphic",
+         size = {800, 600},
+         color = { 255, 255, 255, 255, },
+         textureUV = {0,0,1600,1200},
+         texture = "Data:UI\\NewUI\\Textures\\background.tga",
+      },
    },
-},
 }
