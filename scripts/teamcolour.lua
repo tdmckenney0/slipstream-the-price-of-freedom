ebd439c2fec28f904fa1e60f6f7148028b021d07
Tanner Mckenney <tmckenney7@outlook.com>
2025-01-04 19:20:58 -0500

    Default badges

---
+++ scripts/teamcolour.lua
@@ -0,0 +1,29 @@
+-- LuaDC version 0.9.20
+-- 11/4/2024 10:16:14 PM
+-- LuaDC by Age2uN
+-- on error send source file (compiled lua) and this outputfile to Age2uN@gmx.net
+--
+teamcolours = 
+    { 
+        { 
+            { 0.365, 0.553, 0.667, }, 
+            { 0.8, 0.8, 0.8, }, "DATA:Badges/Hiigaran.tga", }, 
+        { 
+            { 0.9, 0.9, 0.9, }, 
+            { 0.1, 0.1, 0.1, }, "DATA:Badges/Vaygr.tga", }, 
+        { 
+            { 0.9, 0.9, 0.9, }, 
+            { 0.1, 0.1, 0.1, }, "DATA:Badges/Vaygr.tga", }, 
+        { 
+            { 0.9, 0.9, 0.9, }, 
+            { 0.1, 0.1, 0.1, }, "DATA:Badges/Vaygr.tga", }, 
+        { 
+            { 0, 0, 0, }, 
+            { 1, 1, 1, }, "DATA:Badges/sri_corp.tga", }, 
+        { 
+            { 0, 1, 0.5, }, 
+            { 0.1, 0.2, 0.6, }, "DATA:Badges/sri_corp.tga", }, 
+        { 
+            { 0.5, 0.5, 0.5, }, 
+            { 0.7, 0.7, 0.6, }, "DATA:Badges/sri_corp.tga", }, 
+    }
