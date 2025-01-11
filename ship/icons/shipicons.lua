16a8d2e49a98a6215a1c0c74dec3282b4bba207e
Tanner Mckenney <tmckenney7@outlook.com>
2025-01-10 20:04:00 -0500

    vgr_dd_scattershotturret

--- ship/icons/shipicons.lua
+++ ship/icons/shipicons.lua
@@ -1504,6 +1504,7 @@ hgn_bc_production_corvette = Icon_Subsystem_Corvette
 vgr_bc_booster = Icon_Subsystem_Engine
 vgr_c_missilebay = Icon_Subsystem_TorpedoLauncher
 vgr_dd_pulsecannonturret_1 = Icon_Subsystem_IonTurret
+vgr_dd_scattershotturret_1 = Icon_Subsystem_IonTurret
 -- SRI
 sri_commandbase = Vgr_CommStation
 sri_cb_hyperspaceinhibitor = Icon_Subsystem_HyperspaceInhibitor
