87ebe7cc9a970201de0b136ba397df29a5b40443
Tanner Mckenney <5482054+tdmckenney0@users.noreply.github.com>
2026-06-08 18:05:38 -0400

    Update newmainmenu.lua

--- src/ui/newui/main/new/newmainmenu.lua
+++ src/ui/newui/main/new/newmainmenu.lua
@@ -154,7 +154,7 @@ NewMainMenu =
     Text =
     {
         textStyle = "FEHeading3",
-        text = "v4.5",
+        text = "v4.6",
         color = { 0, 0, 0, 255, },
         hAlign = "Center",
     },
