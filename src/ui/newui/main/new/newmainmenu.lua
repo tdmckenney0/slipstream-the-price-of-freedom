d60bf15b4096515f0b7b4d2887a680257d9044a2
Tanner Mckenney <5482054+tdmckenney0@users.noreply.github.com>
2026-06-20 15:57:35 -0400

    bump version number

--- src/ui/newui/main/new/newmainmenu.lua
+++ src/ui/newui/main/new/newmainmenu.lua
@@ -154,7 +154,7 @@ NewMainMenu =
     Text =
     {
         textStyle = "FEHeading3",
-        text = "v4.6",
+        text = "v4.7",
         color = { 0, 0, 0, 255, },
         hAlign = "Center",
     },
